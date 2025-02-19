76d9322256b4f16677be2c126e9c99876fab4c01
SAAD FERID <fersd2018@gmail.com>
2025-02-19 12:55:20 +0100

    xml_finished

---
+++ app/src/main/java/com/example/myapplicationauth/ResetPasswordActivity.java
@@ -0,0 +1,16 @@
+package com.example.myapplicationauth;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ResetPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_reset_password);
+
+    }
+}
