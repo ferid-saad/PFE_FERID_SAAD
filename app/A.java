40b018af435e02058e545038434b7f84d5ad7979
SAAD FERID <fersd2018@gmail.com>
2025-04-18 01:20:38 +0100

    xml_dashboard

---
+++ app/A.java
@@ -0,0 +1,2 @@
+Toast.makeText(this, "failed", Toast.LENGTH_SHORT).show();
+        progressDialog.dismiss();
---
+++ app/src/main/java/com/example/myapplicationauth/DashboardFeridActivity.java
@@ -0,0 +1,16 @@
+package com.example.myapplicationauth;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DashboardFeridActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_dashboard_ferid);
+
+    };
+}
